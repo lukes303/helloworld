7c80f2b4b86b7472bbe6468c67e93bc07614074b
Luke Selby <lukeselby3@gmail.com>
2023-01-24 12:14:01 -0600

    Added 3 hello worlds

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,7 +4,12 @@ public class helloworld {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello");
+		System.out.println("Hello World");
+		
+		for (int i = 0; i < 3; ++i)
+		{
+			System.out.println("Hello World");
+		}
 	}
 	
 }
